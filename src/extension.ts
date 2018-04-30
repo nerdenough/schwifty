c66a2fc3369f150c7351e9e31bb9c343f6c08fab
Brendan Goodenough <brendan@goodenough.nz>
2018-04-30 22:09:02 +1200

    Allow for custom sourcekitten path

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,24 @@
 'use strict';
 import * as vscode from 'vscode';
-import * as cp from 'child_process';
+import { spawn } from 'child_process';
 
 // Formats a specified file
-function format (fileName: String) {
-    cp.execSync(`sourcekitten format --file ${fileName}`);
+function format(sourceKittenPath: string, fileName: string) {
+    spawn(sourceKittenPath, ['format', '--file', fileName])
+        .on('data', (data: any) => console.log(data))
+        .on('error', (err: any) => {
+            console.error(err);
+            vscode.window.showErrorMessage('Schwifty: Error formatting document with SourceKitten', err.message);
+        });
 }
 
 export function activate(context: vscode.ExtensionContext) {
+    const config = vscode.workspace.getConfiguration('schwifty');
+    const sourceKittenPath: string = config.get('sourceKittenPath') || 'sourcekitten';
+
     const formatter = vscode.languages.registerDocumentFormattingEditProvider({ language: 'swift' }, {
         provideDocumentFormattingEdits(document: vscode.TextDocument): any {
-            format(document.fileName);
+            format(sourceKittenPath, document.fileName);
         }
     })
 

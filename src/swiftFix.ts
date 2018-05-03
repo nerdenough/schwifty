2b749fa066d2379494c5a554e39441d1bd0054f7
Brendan Goodenough <brendan@goodenough.nz>
2018-05-03 21:27:39 +1200

    Remove log in swiftFix

--- src/swiftFix.ts
+++ src/swiftFix.ts
@@ -4,7 +4,6 @@ import { spawn } from 'child_process';
 import { getConfig } from './util';
 
 export function autoFix(fileName: string) {
-    console.log(fileName);
     spawn(getConfig('swiftLintPath'), ['autocorrect', '--path', fileName])
         .on('data', (data: any) => console.log(data))
         .on('error', (err: any) => {

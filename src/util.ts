2f08530ae37aca003baaac94b0885d5c314abcaf
Brendan Goodenough <brendan@goodenough.nz>
2018-05-05 14:21:49 +1200

    Fix linting and check errors not displaying at the same time

--- src/util.ts
+++ src/util.ts
@@ -14,76 +14,52 @@ export function getConfig(key: string): any {
     return vscode.workspace.getConfiguration('swift').get(key);
 }
 
-export function handleDiagnosticErrors(document: vscode.TextDocument, errors: ICheckResult[], diagnosticSeverity?: vscode.DiagnosticSeverity) {
-
+export function handleDiagnosticErrors(document: vscode.TextDocument, errors: ICheckResult[]) {
     errorDiagnosticCollection.clear();
     warningDiagnosticCollection.clear();
 
-    let diagnosticMap: Map<string, Map<vscode.DiagnosticSeverity, vscode.Diagnostic[]>> = new Map();
+    const diagnosticMap: Map<string, Map<vscode.DiagnosticSeverity, vscode.Diagnostic[]>> = new Map();
     errors.forEach(error => {
-        // TODO: Handle multiple files, this currently just assumes document
-        // is the correct file.
+        // swiftlint returns <nopath> when using stdin
+        const file = `file://${error.file === '<nopath>' ? document.fileName : error.file}`;
+        const range = getRange(document, error);
+        const severity = getSeverity(error.severity);
+        const diagnostic = new vscode.Diagnostic(range, error.msg, severity);
+        const diagnostics: any = diagnosticMap.get(file) || new Map<vscode.DiagnosticSeverity, vscode.Diagnostic[]>();
 
-        let range = new vscode.Range(error.line - 1, 0, error.line - 1, document.lineAt(error.line - 1).range.end.character + 1);
-        // let text = document.getText(range);
-
-        // let startColumn = 1;
-        // let endColumn = 0;
-
-        // let [, leading, trailing]: any = /^(\s*).*(\s*)$/.exec(text);
-        // if (!error.col) {
-        //     startColumn = leading.length;
-        // } else {
-        //     startColumn = error.col - 1; // range is 0-indexed
-        // }
-        // endColumn = text.length - trailing.length;
-
-        const severity = mapSeverityToVSCodeSeverity(error.severity);
-        let diagnostic = new vscode.Diagnostic(range, error.msg, severity);
-        let diagnostics: any = diagnosticMap.get(error.file);
-        if (!diagnostics) {
-            diagnostics = new Map<vscode.DiagnosticSeverity, vscode.Diagnostic[]>();
-        }
         if (!diagnostics[severity]) {
             diagnostics[severity] = [];
         }
+
         diagnostics[severity].push(diagnostic);
-        diagnosticMap.set(error.file, diagnostics);
+        diagnosticMap.set(file, diagnostics);
     });
 
     diagnosticMap.forEach((diagMap: any, file) => {
-        // swiftlint returns <nopath> when using stdin
-        const fileUri = file === '<nopath>' ? document.uri : vscode.Uri.parse(`file://${file}`);
+        const fileUri = vscode.Uri.parse(file);
+        const errors = diagMap[vscode.DiagnosticSeverity.Error] || [];
+        const warnings = diagMap[vscode.DiagnosticSeverity.Warning] || [];
 
-        if (diagnosticSeverity === undefined || diagnosticSeverity === vscode.DiagnosticSeverity.Error) {
-            const newErrors = diagMap[vscode.DiagnosticSeverity.Error];
-            let existingWarnings = warningDiagnosticCollection.get(fileUri);
-            errorDiagnosticCollection.set(fileUri, newErrors);
+        const errorLines = errors.map((x: vscode.Diagnostic) => x.range.start.line);
+        const filteredWarnings = warnings.filter((x: vscode.Diagnostic) => errorLines.indexOf(x.range.start.line) === -1);
 
-            // Don't diplay warnings if they coincide with an error
-            if (newErrors && existingWarnings) {
-                const errorLines = newErrors.map((x: vscode.Diagnostic) => x.range.start.line);
-                existingWarnings = existingWarnings.filter((x: vscode.Diagnostic) => errorLines.indexOf(x.range.start.line) === -1);
-                warningDiagnosticCollection.set(fileUri, existingWarnings);
-            }
-        }
-
-        if (diagnosticSeverity === undefined || diagnosticSeverity === vscode.DiagnosticSeverity.Warning) {
-            const existingErrors = errorDiagnosticCollection.get(fileUri);
-            let newWarnings = diagMap[vscode.DiagnosticSeverity.Warning];
-
-            // Ignore warnings that coincide with errors
-            if (existingErrors && newWarnings) {
-                const errorLines = existingErrors.map((x: vscode.Diagnostic) => x.range.start.line);
-                newWarnings = newWarnings.filter((x: vscode.Diagnostic) => errorLines.indexOf(x.range.start.line) === -1);
-            }
-
-            warningDiagnosticCollection.set(fileUri, newWarnings);
-        }
+        errorDiagnosticCollection.set(fileUri, errors);
+        warningDiagnosticCollection.set(fileUri, filteredWarnings);
     });
 }
 
-function mapSeverityToVSCodeSeverity(sev: string): vscode.DiagnosticSeverity {
+function getRange(document: vscode.TextDocument, error: ICheckResult) {
+    const initialRange = new vscode.Range(error.line - 1, 0, error.line - 1, document.lineAt(error.line - 1).range.end.character + 1);
+    const text = document.getText(initialRange);
+
+    const [, leading, trailing]: any = /^(\s*).*(\s*)$/.exec(text);
+    const startColumn = leading.length;
+    const endColumn = text.length - trailing.length;
+
+    return new vscode.Range(error.line - 1, startColumn, error.line - 1, endColumn);
+}
+
+function getSeverity(sev: string): vscode.DiagnosticSeverity {
     switch (sev) {
         case 'error': return vscode.DiagnosticSeverity.Error;
         case 'warning': return vscode.DiagnosticSeverity.Warning;
